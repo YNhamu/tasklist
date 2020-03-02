0c447b67085f7ed3ce57530c2b506a6129edb8f6
yuka.nakazawa <taro.kirameki@techacademy.jp>
2020-03-02 21:12:42 +0900

    Modify Index/NewServlet

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -37,8 +38,11 @@ public class IndexServlet extends HttpServlet {
         List<Tasklist> tasklist = em.createNamedQuery("getAllTasklist", Tasklist.class)
                 .getResultList();
 
-        response.getWriter().append(Integer.valueOf(tasklist.size()).toString());
-
         em.close();
+
+        request.setAttribute("tasklist", tasklist);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasklist/index.jsp");
+        rd.forward(request, response);
 
 }}
